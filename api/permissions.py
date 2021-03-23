33b63c0baf1b43da5a7f7bc7c44b688c6a2ef04c
PGluk <glukhov.p@gmail.com>
2021-03-23 20:35:45 +0500

    Added general permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,3 +1,39 @@
-"""
-Here will be api permissions
-"""
+from rest_framework import permissions
+
+from .models import UserRole
+
+MODERATOR_ALLOWED_ACTIONS = ('PATCH', 'DELETE')
+
+
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+                request.user.is_authenticated
+                and request.user.role == UserRole.admin
+                or request.user.is_superuser
+        )
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated
+                and request.user.role == UserRole.moderator)
+
+    def has_object_permission(self, request, view, obj):
+        return (
+                request.method in MODERATOR_ALLOWED_ACTIONS
+                and request.user.role == UserRole.moderator
+                )
+
+
+class IsAuthor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
