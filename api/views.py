fc98545c205662a19f1ddee670122b94bf4a214e
PGluk <glukhov.p@gmail.com>
2021-03-24 23:55:53 +0500

    Added: email_confirmation

--- api/views.py
+++ api/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from .generators import Generator
+from .models import User
+from .permissions import (
+    IsAdmin
+)
+from .serializers import UserSerializer
+
+
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def email_confirmation(request):
+    """
+    Генерирует рендомный username на основе почтового логина
+    """
+    if request.method == 'POST':
+        user_email = request.data.get('email')
+
+        if not user_email:
+            return Response('E-mail не был передан.')
+
+        confirmation_code = Generator().code_generator(length=6)
+        random_username = Generator().login_email_generator(
+            email=user_email,
+            length=5)
+
+        def send_email():
+            send_mail(
+                subject='Yamdb Registration',
+                message=f'Your confirmation code: {confirmation_code}',
+                from_email=settings.EMAIL_FROM,
+                recipient_list=[user_email, ],
+                fail_silently=False
+            )
+
+        if User.objects.filter(email=user_email).exists():
+            user = User.objects.get(email=user_email)
+            serializer = UserSerializer(user, data=request.data,
+                                        partial=True)
+            if serializer.is_valid():
+                serializer.save(confirmation_code=confirmation_code)
+        else:
+            User.objects.create_user(
+                email=user_email,
+                username=random_username,
+                confirmation_code=confirmation_code,
+            )
+
+        send_email()
+        return Response('Код подтверждения был выслан на почту')
+
+
+class GetJWTToken(viewsets.ModelViewSet):
+    pass
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = IsAdmin
+    filter_backends = [filters.SearchFilter]
+    lookup_field = ['username', ]
+    search_fields = ['username', ]
