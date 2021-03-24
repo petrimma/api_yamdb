dcf1d0d6eab7306569c785d9b567cc3028693090
PGluk <glukhov.p@gmail.com>
2021-03-24 23:55:11 +0500

    fix

--- api/models.py
+++ api/models.py
@@ -35,7 +35,10 @@ class User(AbstractUser):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
         ordering = ('username',)
-        
+
+    def __str__(self):
+        return User.username
+
 
 class Title(models.Model):
     pass
