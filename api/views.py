1abc7e71588ff97d04dfc5df645abfb43eca209f
Rimma <tetradir@yandex.ru>
2021-03-26 20:17:17 +0300

    Add views for Comment and Review

--- api/views.py
+++ api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Review, Title
+from .permissions import ReadOnly, IsAdmin, IsModerator, IsAuthor
+from .serializers import CommentSerializer, ReviewSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = ReadOnly, IsAuthenticated, IsAuthor, IsModerator
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        serializer.save(author=self.request.user, review=review)
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        return review.comments.all()
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = ReadOnly, IsAuthenticated, IsAuthor, IsModerator
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
+        serializer.save(author=self.request.user, title=title)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
+        return title.reviews.all()
